00a098fe96dc166bbf4ee64395826387e3a04b64
Israel Sanchez <israel.pro1994@gmail.com>
2019-08-08 02:34:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,13 @@
 class Dog
   
   def name=(name)
-    
-   puts @name = name
+    puts @name = name
     end
   
     def name
       @name
+    end
+    
   def breed=(breed)
     
    puts breed = @breed
